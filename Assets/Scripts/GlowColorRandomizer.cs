e29a26f2f895bbf32e978c5d7b2d3797553948e8
Artyom Sovetnikov <elringus@yandex.ru>
2017-06-11 01:34:14 +0300

    2.0 initial update
    Restructured the project; moved SpriteGlow core assets to a separate
    folder.
    Added performance testing scene.
    Improved script and shader performance.
    Fixed the issue when outline wasn't drawing at the sprite borders.
    Fixed Unity 5.6 deprecated API warnings.

---
+++ Assets/Scripts/GlowColorRandomizer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Used to break batching when testing performance.
+/// </summary>
+[RequireComponent(typeof(SpriteGlow))]
+public class GlowColorRandomizer : MonoBehaviour
+{
+    private SpriteGlow spriteRenderer;
+
+    private void Awake ()
+    {
+        spriteRenderer = GetComponent<SpriteGlow>();
+    }
+
+    private void Start ()
+    {
+        spriteRenderer.GlowColor = Random.ColorHSV();
+    }
+}
