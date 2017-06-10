8b9591bbe30ed4f4eaf43f15a27d7432530b4da4
Artyom Sovetnikov <elringus@yandex.ru>
2017-06-11 01:53:15 +0300

    fix formatting

--- Assets/SpriteGlow/SpriteGlow.cs
+++ Assets/SpriteGlow/SpriteGlow.cs
@@ -3,38 +3,38 @@
 [ExecuteInEditMode, RequireComponent(typeof(SpriteRenderer))]
 public class SpriteGlow : MonoBehaviour
 {
-	[ColorUsage(true, true, 0f, 8f, 0.125f, 3f)]
-	public Color GlowColor = Color.white;
+    [ColorUsage(true, true, 0f, 8f, 0.125f, 3f)]
+    public Color GlowColor = Color.white;
 
-	[Range(0, 160)]
-	public int OutlineWidth = 1;
+    [Range(0, 160)]
+    public int OutlineWidth = 1;
 
-	private SpriteRenderer spriteRenderer;
+    private SpriteRenderer spriteRenderer;
     private MaterialPropertyBlock materialProperties;
 
     private void OnEnable ()
-	{
+    {
         spriteRenderer = GetComponent<SpriteRenderer>();
         materialProperties = new MaterialPropertyBlock();
         UpdateOutline(true);
-	}
+    }
 
-	private void OnDisable ()
-	{
-		UpdateOutline(false);
-	}
+    private void OnDisable ()
+    {
+        UpdateOutline(false);
+    }
 
-	private void Update ()
-	{
+    private void Update ()
+    {
         UpdateOutline(true);
-	}
+    }
 
-	private void UpdateOutline (bool isOutlineEnabled)
-	{
-		spriteRenderer.GetPropertyBlock(materialProperties);
+    private void UpdateOutline (bool isOutlineEnabled)
+    {
+        spriteRenderer.GetPropertyBlock(materialProperties);
         materialProperties.SetFloat("_IsOutlineEnabled", isOutlineEnabled ? 1f : 0f);
         materialProperties.SetColor("_OutlineColor", GlowColor);
         materialProperties.SetFloat("_OutlineSize", OutlineWidth);
-		spriteRenderer.SetPropertyBlock(materialProperties);
-	}
+        spriteRenderer.SetPropertyBlock(materialProperties);
+    }
 }
